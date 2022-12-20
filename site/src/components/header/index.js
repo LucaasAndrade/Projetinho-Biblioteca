df4134ce7c948ef6887a86dbf6eaf38383bddb96
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2022-12-20 14:19:07 -0300

    FRONT Componente Header OK

--- site/src/components/header/index.js
+++ site/src/components/header/index.js
@@ -5,6 +5,28 @@ import './index.scss'
 
 export default function ComponenteHeader() {
     return (
-        <header>grgrt</header>
+        <header className='componente-header'>
+            <div className='opcoes'>
+                <div className='opcao'>
+                    <img src='/assets/images/inicio.png' />
+                    <p>Tela principal</p>
+                </div>
+                <div className='opcao'>
+                    <img src='/assets/images/mais.png' />
+                    <p>Adicionar livros</p>
+                </div>
+                <div className='opcao'>
+                    <img src='/assets/images/perfil.png' />
+                    <p>Cadastrar leitor</p>
+                </div>
+                <div className='opcao'>
+                    <img src='/assets/images/menu.png' />
+                    <p>Empréstimos</p>
+                </div>
+            </div>
+            <div>
+                <img className='logo' src='/assets/images/logo.png' />
+            </div>
+        </header>
     )
 }
