2db08665ce0310b241c7ddbd0bcffa8fc8275a95
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2022-12-20 15:18:06 -0300

    FRONT Principal OK

--- site/src/pages/principal/index.js
+++ site/src/pages/principal/index.js
@@ -6,8 +6,28 @@ import ComponenteHeader from '../../components/header'
 
 export default function Principal() {
     return (
-        <main>
+        <main className='principal'>
             <ComponenteHeader />
+            <section className='info-principal'>
+                <div className='fundo'>
+                    <div className='mensagem'>
+                        <p>Olá <span>@Admin</span>, atualmente temos:</p>
+                    </div>
+                    <div className='fundo-livros'>
+                        <div>
+                            <div className='div-livro'>
+                                <p>Livros emprestados: <span>6</span></p>
+                            </div>
+                            <div className='div-livro'>
+                                <p>Livros atrasados: <span className='spn-atrasado'>0</span></p>
+                            </div>
+                        </div>
+                        <div className='div-livro'>
+                            <p>Livros para recolher hoje: <span>2</span></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
