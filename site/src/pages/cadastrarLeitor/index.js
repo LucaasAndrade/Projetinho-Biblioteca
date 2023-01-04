a885c18f7c48c704a7abc37702befb6172a69d56
Rayssa Rodrigues <rayssa.rodrigues1198@gmail.com>
2023-01-04 11:46:52 -0300

    FRONT cadastro leitor OK

---
+++ site/src/pages/cadastrarLeitor/index.js
@@ -0,0 +1,55 @@
+import './index.scss';
+
+import ComponenteHeader from '../../components/header';
+
+
+export default function CadastrarLeitor() {
+    return(
+        <main className='cadastrar'>
+            <ComponenteHeader />
+            <section className='info-cadastro'>
+                <div className='fundo-cadastro'>
+                    <div className='fundo-imagem'>
+                        <img src='/assets/images/add-imagem.png' />
+                    </div>
+                    <div className='fundo-info'>
+                        <p>Nome:</p>
+                        <input className='inp-nome' type='text' placeholder='EXEMPLO' />
+                        <div className='col-dois'>
+                            <div>
+                                <p>Curso:</p>
+                                <select>
+                                    <option value="" selected disabled hidden>Curso</option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                </select>
+                            </div>
+                            <div className='div-col-dois'>
+                                <p>Turma:</p>
+                                <select>
+                                    <option value=""  selected disabled hidden>Turma</option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                    <option value="??"> ?? </option>
+                                </select>
+                            </div>
+                            <div className='div-col-dois'>
+                                <p>Telefone p/ contato:</p>
+                                <input type='tell' placeholder='(xx) xxxxx-xxxx'/>
+                            </div>
+                        </div>
+                        <div className="textarea">
+                            <p>Observações:</p>
+                            <textarea className="descricao" name="story" rows="7" cols="46" placeholder='EXEMPLO'></textarea>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
--- site/src/routes.js
+++ site/src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login'
 import Principal from './pages/principal';
+import CadastrarLeitor from './pages/cadastrarLeitor';
 
 export default function Index() {
     return (
@@ -9,6 +10,7 @@ export default function Index() {
             <Routes>
                 <Route path='/' element={<Login />} />
                 <Route path='/home' element={<Principal />} />
+                <Route path='/cadastro/leitor' element={<CadastrarLeitor />} />
             </Routes>
         </BrowserRouter>
     )
